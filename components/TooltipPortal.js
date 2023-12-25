715483bf8fb971e67288422b50006b5054e2739f
Nggohel <gohelnidhi15898@gmail.com>
2023-12-25 19:08:59 +0530

    add Logic

--- components/TooltipPortal.js
+++ components/TooltipPortal.js
@@ -1,9 +1,26 @@
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const portalRoot = typeof document !== 'undefined' ? document.getElementById('tooltip-portal') : null;
+const portalRoot =
+  typeof document !== "undefined"
+    ? document.getElementById("tooltip-portal")
+    : null;
 
 function TooltipPortal({ children }) {
+  const portalElement = React.useRef(null);
 
+  if (!portalElement.current) {
+    portalElement.current = document.createElement("div");
+    portalRoot.appendChild(portalElement.current);
+  }
+
+  React.useEffect(() => {
+    return () => {
+      portalRoot.removeChild(portalElement.current);
+    };
+  }, []);
+
+  return ReactDOM.createPortal(children, portalElement.current);
 }
 
 export default TooltipPortal;
